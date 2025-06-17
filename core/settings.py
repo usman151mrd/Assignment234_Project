e471896c20c4c14707b89f52323fe5e5d04b008e
HYE <monumhashmi1@gmail.com>
2025-06-17 03:51:06 -0700

    configure project

--- core/settings.py
+++ core/settings.py
@@ -201,3 +201,11 @@ STATICFILES_DIRS = [
 SIMPLE_JWT = {
    'AUTH_HEADER_TYPES': ('Bearer',),
 }
+
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+    # `allauth` specific authentication methods, such as login by email
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
--- resume_builder/web/views.py
+++ resume_builder/web/views.py
@@ -7,7 +7,7 @@ from resume_builder.forms import WorkExperienceForm
 
 class WorkExperienceListView(LoginRequiredMixin, ListView):
     model = WorkExperience
-    template_name = 'work_experience_list.html'
+    template_name = 'resume_builder/work_experience/work_experience_list.html'
     context_object_name = 'experiences'
 
     def get_queryset(self):
@@ -16,7 +16,7 @@ class WorkExperienceListView(LoginRequiredMixin, ListView):
 class WorkExperienceCreateView(LoginRequiredMixin, CreateView):
     model = WorkExperience
     form_class = WorkExperienceForm
-    template_name = 'work_experience_form.html'
+    template_name = 'resume_builder/work_experience/work_experience_form.html'
     success_url = reverse_lazy('work_experience_list')
 
     def form_valid(self, form):
@@ -30,7 +30,7 @@ class WorkExperienceCreateView(LoginRequiredMixin, CreateView):
 class WorkExperienceUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = WorkExperience
     form_class = WorkExperienceForm
-    template_name = 'work_experience_form.html'
+    template_name = 'resume_builder/work_experience/work_experience_form.html'
     success_url = reverse_lazy('work_experience_list')
 
     def test_func(self):
@@ -38,7 +38,7 @@ class WorkExperienceUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateVi
 
 class WorkExperienceDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = WorkExperience
-    template_name = 'work_experience_confirm_delete.html'
+    template_name = 'resume_builder/work_experience/work_experience_confirm_delete.html'
     success_url = reverse_lazy('work_experience_list')
 
     def test_func(self):
@@ -46,7 +46,7 @@ class WorkExperienceDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteVi
 
 class WorkExperienceDetailView(LoginRequiredMixin, UserPassesTestMixin, DetailView):
     model = WorkExperience
-    template_name = 'work_experience_detail.html'
+    template_name = 'resume_builder/work_experience/work_experience_detail.html'
     context_object_name = 'experience'
 
     def test_func(self):
